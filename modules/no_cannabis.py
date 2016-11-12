2ffb5e65523f7cbfe3e78d74a29e23e55d734e0b
psy <psy@darmstadt.ccc.de>
2016-11-12 20:27:41 +0100

    initial

---
+++ modules/no_cannabis.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+from willie.module import rate, rule, interval
+from random import randint
+from datetime import datetime, timedelta
+
+
+def setup(bot):
+    if 'cc' not in bot.memory:
+        print('initialising memory')
+        bot.memory['cc'] = {}
+    return
+
+
+def shutdown(bot):
+    return
+
+
+@rule('.*hola.*')
+@rule('.*cann?abis.*')
+@rule('.*weed.*')
+def no_cannabis(bot, trigger):
+    if trigger.sender.startswith('#'):
+        if trigger.nick not in bot.memory['cc']:
+            bot.memory['cc'][trigger.nick] = {'dt': datetime.now(),
+                                              'count': 1}
+            bot.reply('No cannabis!')
+        elif datetime.now() - bot.memory['cc'][trigger.nick]['dt'] > timedelta(minutes=10):
+            bot.memory['cc'][trigger.nick]['count'] += 1
+            bot.memory['cc'][trigger.nick]['dt'] = datetime.now()
+            bot.reply('NO CANNABIS!')
+        else:
+            bot.memory['cc'][trigger.nick]['count'] += 0.5
+
+        if bot.memory['cc'][trigger.nick]['count'] > 2:
+            if randint(2, 5) <= bot.memory['cc'][trigger.nick]['count']:
+                bot.write(['KICK', trigger.sender, trigger.nick], 'ETOOMUCHCANNABIS')
+
+
+@rule('.*Por\s?que.*\?')
+@rule('.*why.*\?')
+@rule('.*(wieso|warum|weshalb).*\?')
+@rate(10 * 60)
+def why(bot, trigger):
+    if trigger.nick in bot.memory['cc'] and datetime.now() - bot.memory['cc'][trigger.nick]['dt'] < timedelta(
+            minutes=5):
+        bot.reply('This channel is related to a conference of computer science students, not cannabis! That\'s why!')
+
+
+@interval(3600)
+def lower_score(bot):
+    for nick in list(bot.memory['cc']):
+        bot.memory['cc'][nick]['count'] -= 1
+
+        if bot.memory['cc'][nick]['count'] <= 0:
+            bot.memory['cc'].pop(nick)
---
+++ modules/polly.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+from willie.module import rate, rule, interval
+from random import randint
+from datetime import datetime, timedelta
+from random import random, randint, choice
+
+def setup(bot):
+    if 'polly' not in bot.memory:
+        bot.memory['polly'] = {}
+        bot.memory['polly']['last_sentences'] = []
+        bot.memory['polly']['talk_next_time'] = False
+        bot.memory['polly']['sentences_since_last_answer'] = 0
+        bot.memory['polly']['start_threshold'] = 0
+
+@rule('.*')
+def on_msg(bot, trigger):
+    if not trigger.sender.startswith('#'):
+        return
+
+    if trigger.sender is not 'polly':
+        bot.memory['polly']['last_sentences'] = bot.memory['polly']['last_sentences'][-9:].append(trigger.group(0))
+        bot.memory['polly']['sentences_since_last_answer'] += 1
+
+    if random() > 0.975:
+        bot.memory['polly']['talk_next_time'] = True
+
+@interval(23)
+def cron(bot):
+    answers = ['Hinter dir, ein dreiköpfiger Affe!', 'Und ne Buddel voll Rum.', 'Du kämpfst wie ein dummer Bauer!',
+               'Mr Cottons Papagei, die selbe Frage!', 'Polly Cracker?', 'Klar soweit?', 'Arrrrrrrr!',
+               'Ich bin ein mächtiger Pirat!', 'Polly will Cracker!']
+
+    rand = random()
+    threshold = bot.memory['polly']['start_threshold'] + 0.0625 * bot.memory['polly']['sentences_since_last_answer']
+
+    if (bot.memory['polly']['talk_next_time'] or rand < threshold):
+        if bot.memory['polly']['talk_next_time']:
+            bot.memory['polly']['talk_next_time'] = False
+        else:
+            bot.memory['polly']['sentences_since_last_answer'] = 0
+            bot.memory['polly']['start_threshold'] = -1 * random()
+
+        if random() > 0.8:
+            answer = choice(answers)
+        else:
+            answer = choice(bot.memory['polly']['last_sentences'])
+
+        bot.msg('#kif', pollyfier(answer))
+
+
+def pollyfier(msg, nested=False):
+    output = ''
+
+    words = msg.split(' ')
+
+    for i, word in enumerate(words):
+        for j, char in enumerate(word):
+            output += char
+            if char.lower() == 'r' and random() > 0.8:
+                output += 'r' * randint(1, 5)
+
+        output += ' '
+
+        if not nested and random() > 0.8:
+           output += pollyfier(choice([words[i], '*croack*', '*rrack*', 'Arrrrr!']), nested=True)
+
+    return output
